19e87379507a4bb1915a6029c14ba7495d357329
sonamdhital <sonamdhital4050@gmail.com>
2026-09-02 11:39:16 +0545

    added

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<?php
+<?php 
 require_once 'header.php';
+require_once 'connection.php';
+
+$sql="SELECT * FROM products";
+$result=mysqli_query($conn,$sql);
 ?>
 
-<h1>
-    Welcome to our sites
-</h1>
+<h1>Product List</h1>
 
-<?php
+<ul>
+    <?php while($row = mysqli_fetch_assoc($result)): ?>
+        <li>
+            <img src="uploads/<?php echo $row['image']?>" width="200" height="200" alt="">
+            <h2><?php echo $row['title']; ?></h2>
+            <p><?php echo $row['description']; ?></p>
+            <p>Price: $<?php echo $row['price']; ?></p>
+        </li>
+    <?php endwhile; ?>
+</ul>
+
+<?php 
 require_once 'footer.php';
 ?>
-
